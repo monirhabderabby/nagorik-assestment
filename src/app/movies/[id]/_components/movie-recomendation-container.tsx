cf6a884df013894724355e8fae34c0c7236dab26
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-24 20:13:26 +0600

    feat: add movie recommendations to details page
    - Fetched movie recommendations from TMDB API
    - Integrated recommendations into MovieDetailsContainer
    - Added error handling for failed recommendation fetches
    - Cached recommendations along with movie details for 60 seconds

---
+++ src/app/movies/[id]/_components/movie-recomendation-container.tsx
@@ -0,0 +1,22 @@
+import MovieRecomendationCard from "@/components/ui/movie-recomendation-card";
+import { movieCardSchemaType } from "@/schemas/movie.schema";
+
+interface Props {
+  recomendations?: movieCardSchemaType[];
+}
+
+const MovieRecomendationContainer = ({ recomendations }: Props) => {
+  return (
+    <div className="container">
+      <h1 className="text-[26px]">Recommendation</h1>
+
+      <div className="w-full flex flex-wrap mt-3 gap-y-10 gap-x-5">
+        {recomendations?.slice(0, 10).map((movie: movieCardSchemaType) => (
+          <MovieRecomendationCard key={movie.id} data={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieRecomendationContainer;
